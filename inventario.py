3fcb745846a8a43102865a0e76f41b5600b0a58e
Oscar Tambraico <oscartambrai@gmail.com>
2026-02-24 10:23:42 -0500

    Mi primer commit

---
+++ inventario.py
@@ -0,0 +1,6 @@
+# Se define 3 variables para un artículo
+nombre_articulo = input("Ingrese el nombre del articulo: ")
+precio_unitario = input("Ingrese el precio del articulo: ")
+stock_actual = input("Ingrese la cantidad del articulo: ")
+valor_total = precio_unitario*stock_actual
+print("El precio total del articulo es: ", valor_total)
