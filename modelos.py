12ffdde39dae7e6ddd8ac187bc348dc502d4b518
Oscar Tambraico <oscartambrai@gmail.com>
2026-02-27 10:55:59 -0500

    Creada clase Producto con métodos de venta y reposición

---
+++ modelos.py
@@ -0,0 +1,31 @@
+# Definiendo nuestra Clase
+class Producto:
+    def __init__(self, nombre, precio, stock):
+        # Atributos (Características)
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+
+    def mostrar_info(self):
+        return f"Producto: {self.nombre} | Precio: ${self.precio} | Stock: {self.stock}"
+
+    def vender(self, cantidad):
+        if cantidad <= self.stock:
+            self.stock -= cantidad
+            print(f"✅ Venta realizada: {cantidad} unidades de {self.nombre}")
+        else:
+            print(f"❌ Error: No hay suficiente stock de {self.nombre}")
+    
+    def reposicionar_stock(self, cantidad):
+        self.stock += cantidad
+        print(f"el stock actual es {self.stock}")
+
+# --- Probando nuestra Clase ---
+laptop = Producto("MacBook Air", 1200, 5)
+print(laptop.mostrar_info())
+
+laptop.vender(2)
+print(laptop.mostrar_info())
+
+celular = Producto("iPhone", 800, 10)
+print(celular.reposicionar_stock(5))
