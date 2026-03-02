d5cbdb7f4f33726c4e4b99887a16eb7b74e48d34
Oscar Tambraico <oscartambrai@gmail.com>
2026-03-02 08:25:37 -0500

    Añadida funcionalidad para crear productos desde la web

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template    
+from flask import Flask, render_template, request, redirect    
 import sqlite3
 
 app = Flask(__name__)
@@ -20,6 +20,28 @@ def home():
 def lista_productos():
     datos = obtener_datos() # La función que ya tenías de SQLite
     return render_template('index.html', productos=datos)
+
+@app.route('/nuevo', methods=['GET', 'POST'])
+def nuevo_producto():
+    if request.method == 'POST':
+        # 1. Obtener datos del formulario
+        nombre = request.form['nombre']
+        precio = float(request.form['precio'])
+        stock = int(request.form['stock'])
+
+        # 2. Guardar en la Base de Datos
+        conexion = sqlite3.connect("tienda.db")
+        cursor = conexion.cursor()
+        cursor.execute("INSERT INTO productos (nombre, precio, stock) VALUES (?, ?, ?)", 
+                       (nombre, precio, stock))
+        conexion.commit()
+        conexion.close()
+
+        # 3. Redirigir al catálogo para ver el cambio
+        return redirect('/productos')
+    
+    # Si es GET, simplemente mostramos el formulario
+    return render_template('nuevo_producto.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
